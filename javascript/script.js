b7b6a7c40994dc940c68131bf94c17aee26da792
Brock <zyuan18@my.bcit.ca>
2023-03-08 14:38:38 -0800

    check code property and display whether found or push to courseList

--- javascript/script.js
+++ javascript/script.js
@@ -11,9 +11,22 @@ let course3 = {
     name:"Technical Math for IT",
 }
 const courseList = [course1,course2,course3];
-
+let code;
 do {
     code=prompt("Enter a 4-digit code");
 }
 while(isNaN(code) || code.length != 4);
 
+for (let course of courseList){
+    if(course.code == code){
+        console.log(`Yes I am taking the course ${course.code} - ${course.name}`);
+    }
+    else{
+        let newCourse = {
+            code,
+            name: null,
+        }
+        courseList.push(newCourse);
+        console.log(courseList);
+    }
+}
