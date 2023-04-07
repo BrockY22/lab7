1fae675338a29bea66bd705040c9ad012cc8ed3f
Brock <zyuan18@my.bcit.ca>
2023-04-06 17:39:36 -0700

    created findCourse function and fixed courses html

--- javascript/script.js
+++ javascript/script.js
@@ -1,48 +1,10 @@
-// let course1 = {
-//     code: "ACIT 1630",
-//     name: "Database Systems",
-// }
-// let course2 = {
-//     code: "ACIT 1420",
-//     name: "Intro to Systems Admin",
-// }
-// let course3 = {
-//     code:"MATH 1310",
-//     name:"Technical Math for IT",
-// }
-// const courseList = [course1,course2,course3];
-// let code;
-// do {
-//     code=prompt("Enter a 4-digit code");
-// }
-// while(isNaN(code) || code.length != 4);
-
-// let courseFound = false;
-// for (let course of courseList){
-//     if(course.code.includes(code)){
-//         console.log(`Yes I am taking the course ${course.code} - ${course.name}`);
-//         courseFound = true;
-//         break;
-//     }
-// }
-// if(!courseFound){
-//     let newCourse = {
-//         code,
-//         name: null,
-//     }
-//     courseList.push(newCourse);
-//     console.log(courseList);
-// }
-
+const courseRef = document.querySelector('.courseList');
+const liRef = document.querySelectorAll('.courseList li');
+const titleList = courseRef.querySelectorAll('li p a');
+const dateList = courseRef.querySelectorAll('li p:nth-child(2)');
 
 function createCourseArray(){
     let courseArray=[];
-  
-    const courseRef = document.querySelector('.courseList');
-    const titleList = courseRef.querySelectorAll('li p a');
-    const dateList = courseRef.querySelectorAll('li p:nth-child(2)');
-    
-  
     for(let i=0;i < titleList.length;i++){
         let course = {
             code:"",
@@ -52,9 +14,49 @@ function createCourseArray(){
         course.date = dateList[i].textContent;
         courseArray.push(course);
     }
-    for(course of courseArray){
-        console.log(course);
-    }
     return courseArray;
 }
-createCourseArray()
+
+function findCourse(courseList){
+    let code;
+    let validateCode;
+    do {
+        validateCode=true;
+        code=prompt("Enter a 4-digit code");
+        if(code == null)    {
+            break;
+        }
+        else if(isNaN(code) || code.length != 4){
+            validateCode=false;
+            alert("Not a valid code, please try again!");
+        }
+    }
+    while(!validateCode);
+    if(code != null){
+        let courseFound = false;
+        for (let course of courseList){
+            if(course.code.includes(code)){
+                for(let i = 0; i < liRef.length; i++){
+                    if(titleList[i].textContent == course.code )
+                        liRef[i].classList.add('changeBGGreen');
+                }
+                courseFound = true;
+                break;
+            }
+        }
+        if(!courseFound){
+            const newLi = document.createElement('li');
+            const newP1 = document.createElement('p');
+            const newP2 = document.createElement('p');
+            const newP3 = document.createElement('p');
+            newP1.textContent = code;
+            newP2.textContent = 'Fall 2020';
+            newP3.textContent = 'N/A';
+            newLi.appendChild(newP1);
+            newLi.appendChild(newP2);
+            newLi.appendChild(newP3);
+            courseRef.appendChild(newLi);    
+        }
+    }
+}
+findCourse(createCourseArray())
